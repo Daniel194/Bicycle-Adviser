49fa0cfbd05e49efacb7c8365a5560948ad1cb76
Daniel194 <lungu.daniel94@gmail.com>
2016-06-08 23:53:51 +0300

    Add View, CSS and AngularJS

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app',[]);
+
+app.controller('appController',function ($scope) {
+    $scope.first = 0;
+    $scope.second = 0;
+
+    $scope.calculate = function () {
+        $scope.result = $scope.first + $scope.second;
+    };
+
+    $scope.getQuestion = function () {
+        return "view/question.html"
+    };
+});
