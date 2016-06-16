64be3060e7cad3a5b8984d65e90cd7e308358fcf
Daniel194 <lungu.daniel94@gmail.com>
2016-06-16 23:11:22 +0300

    Display all questions.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,17 +2,17 @@ var app = angular.module('app', [])
     .controller('appController', function ($scope, $http) {
         var url = "http://localhost:8080/ws/question/all";
         var count = 0;
-
-        $scope.getQuestion = function () {
-            return 'view/question.html';
-        };
+        var questions = [];
 
         $http.get(url).success(
             function (data) {
-                $scope.questions = data;
+                questions = data;
                 $scope.question = data[count];
             }
         );
 
+        $scope.answer = function (valeu) {
+            $scope.question = questions[count++];
+        };
 
     });
