f5dc06f26e2fb3c081896e729cb7cb2b63f0789b
Daniel194 <lungu.daniel94@gmail.com>
2016-06-17 00:09:26 +0300

    Separate answer and question view.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,18 +1,26 @@
 var app = angular.module('app', [])
     .controller('appController', function ($scope, $http) {
-        var url = "http://localhost:8080/ws/question/all";
+        var urlQuestion = "http://localhost:8080/ws/question/all";
         var count = 0;
         var questions = [];
+        var responses = [];
+        $scope.showQuestion = true;
 
-        $http.get(url).success(
+        $http.get(urlQuestion).success(
             function (data) {
                 questions = data;
                 $scope.question = data[count];
             }
         );
 
-        $scope.answer = function (valeu) {
-            $scope.question = questions[count++];
+        $scope.answer = function (answer) {
+            responses.push({"value": questions[count].value, "answer": answer});
+
+            if (count < questions.length - 1)
+                $scope.question = questions[++count];
+            else
+                $scope.showQuestion = false;
+
         };
 
     });
