ffc337331b428b6f12a7e4601a09479d5e2bb5b3
Daniel194 <lungu.daniel94@gmail.com>
2016-06-17 23:54:31 +0300

    Convert Rules file from text to JSON

--- src/main/java/com/bicycle/adviser/utility/Constant.java
+++ src/main/java/com/bicycle/adviser/utility/Constant.java
@@ -3,4 +3,5 @@ package com.bicycle.adviser.utility;
 
 public class Constant {
     public static final String questionPath = "/Users/daniellungu/Documents/Workspace/Java/Bicycle-Adviser/src/main/resource/question.json";
+    public static final String rulePath = "/Users/daniellungu/Documents/Workspace/Java/Bicycle-Adviser/src/main/resource/rule.json";
 }
